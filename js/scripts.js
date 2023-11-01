bd48982c97617c5d4933bf30827e40c8ce358c9b
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-01 10:36:23 -0700

    Define global variables and add prototype to ScoreTally that allows for scores to be tracked
    Co-authored-by: Hunter Empey <LizardMak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@ function ScoreTally() {
   this.scores = {};
 }
 
+ScoreTally.prototype.trackScore = function(playerScore) {
+  this.scores[playerScore.playerNumber] = playerScore;
+}
 
 // Business Logic for PlayerScore constructor
 
@@ -19,7 +22,38 @@ PlayerScore.prototype.addScore = function(input) {
 }
 
 
+// Define Global Variables
+let gameScoreTally = new ScoreTally;
+let p1Score = new PlayerScore(1,0)
+let p2Score = new PlayerScore(2,0)
+gameScoreTally.trackScore(p1Score);
+gameScoreTally.trackScore(p2Score);
+
+
 // Business Logic
+function playerTurn(playerNumber) {
+  let keepRolling = 1;
+  let turnTotal = 0;
+  for(let i = 0; i < keepRolling; i++) {
+    let diceRoll = Math.floor(Math.random() * 6) +1;    
+    if (diceRoll === 1) {
+      keepRolling = 0;
+    } else if (diceRoll > 1) {
+      
+  
+      //(event listener for reroll)
+      turnTotal = turnTotal + diceRoll
+      //(event listener for hold)
+      gameScoreTally.scores[playerNumber].addScore(turnTotal)
+    }
+  gameScoreTally.scores[playerNumber].prototype.addScore(turnTotal)
+  turnTotalP1 = 0
+  
+
+  }
+}
+
+
 
 
 
