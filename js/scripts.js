f5f2039422d3dcfa645c9fc3dad0750e0303217f
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-01 14:08:18 -0700

    Make sure that roll dice and hold functions are working as expected.
    Co-authored-by: Hunter Empey <LizardMak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -28,47 +28,92 @@ let p1Score = new PlayerScore(1,0)
 let p2Score = new PlayerScore(2,0)
 gameScoreTally.trackScore(p1Score);
 gameScoreTally.trackScore(p2Score);
-let currentTurn = 1;
+let currentTurn = 2;
+let turnTotal = 0;
 
 
 // Business Logic
 function changePlayerTurn() {
+  event.preventDefault();
+  displayScores()
+  turnTotal = 0;
   if (currentTurn === 1) {
     currentTurn = 2;
-    playerTurn(2);
+    playerTurn();
+    displayCurrentPlayerTurn()
+    return
   } else {
     currentTurn = 1;
-    playerTurn(1);
+    playerTurn();
+    displayCurrentPlayerTurn()
+    return
   }
 }
 
-function playerTurn(playerNumber) {
-  let keepRolling = 1;
-  let turnTotal = 0;
-  for(let i = 0; i < keepRolling; i++) {
-    console.log("Number of Roll: " + i);
-    let diceRoll = Math.floor(Math.random() * 6) +1;
-    console.log("Value of Dice: " + diceRoll);
-    if (diceRoll === 1) {
-      keepRolling = 0;
-      changePlayerTurn();
-    } else if (diceRoll > 1) {
+function playerTurn() {
+        event.preventDefault();
+        let diceRollTotal = rollDice(currentTurn);
+        turnTotal += diceRollTotal;
+        console.log(turnTotal);
+        displayTurnTotal(turnTotal);
       
-      turnTotal = turnTotal + diceRoll
-      //(event listener for reroll)
-      keepRolling = keepRolling + 1;
-      //(event listener for hold)
-      turnTotal = turnTotal + diceRoll
-      gameScoreTally.scores[playerNumber].addScore(turnTotal)
-      keepRolling = i-1;
-      changePlayerTurn();
-    }
-  gameScoreTally.scores[playerNumber].addScore(turnTotal)
+}
+
+
+
+
+function rollDice() {
+  event.preventDefault();
+  let diceRoll = Math.floor(Math.random() * 6) +1;
+  if (diceRoll === 1) {
+    holdDice(currentTurn,0);
+  } else if (diceRoll > 1) {
+    displayRolledNumber(diceRoll);
+    return diceRoll;
   }
 }
 
+function holdDice() {
+  event.preventDefault();
+  gameScoreTally.scores[currentTurn].addScore(turnTotal)
+  changePlayerTurn();
+}
 
 
 
 
 // UI Logic
+
+function displayScores() {
+  let p1ScoreDisplay = document.getElementById("displayScoreP1");
+  let p2ScoreDisplay = document.getElementById("displayScoreP2");
+  let p1Score = gameScoreTally.scores[1].currentScore;
+  let p2Score = gameScoreTally.scores[2].currentScore;
+  p1ScoreDisplay.innerText = p1Score;
+  p2ScoreDisplay.innerText = p2Score;
+}
+
+function displayRolledNumber(rolledNumber) {
+  let rolledNumberDisplay = document.getElementById("rolledNumberDisplay");
+  rolledNumberDisplay.innerText = "You rolled a " + rolledNumber + ".";
+}
+
+function displayTurnTotal(turnTotal) {
+  let turnTotalDisplay1 = document.getElementById("turnTotalDisplay1");
+  let turnTotalDisplay2 = document.getElementById("turnTotalDisplay2");
+  turnTotalDisplay1.innerHTML = "Your current total for this turn is " + turnTotal + "." + "Would you like to Roll Dice again, or Hold your current score?";
+  turnTotalDisplay2.innerHTML = "Adding this total to your current score would bring you to " + (parseInt(turnTotal) + parseInt(gameScoreTally.scores[currentTurn].currentScore)) + ".";
+}
+
+function displayCurrentPlayerTurn() {
+  let currentTurnDisplay = document.getElementById("currentPlayerTurn");
+  let turn = currentTurn;
+  currentTurnDisplay.innerText = "Player " + currentTurn + "'s Turn!";
+  
+}
+
+window.addEventListener("load", function() {
+  this.document.getElementById("playGame").addEventListener("click",changePlayerTurn);
+  this.document.getElementById("hold").addEventListener("click", holdDice);
+  this.document.getElementById("rollDice").addEventListener("click", playerTurn);
+})
